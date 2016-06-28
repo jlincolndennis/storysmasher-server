ec84f9823d3a0e9639124e087c557041500d9c3e
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-06-28 16:39:00 -0600

    CORS I WILL MASTER YOUUUUUUUU

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://storysmasher.firebaseapp.com/', 'localhost:3000'
+    origins 'https://storysmasher.firebaseapp.com/', 'http://localhost:5000'
 
     resource '*',
       headers: :any,
