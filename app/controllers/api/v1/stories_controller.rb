24097758e7fe65553018e3c49e0fee3ceb9d6d0a
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-06-28 18:22:34 -0600

    POST routes workin!

--- app/controllers/api/v1/stories_controller.rb
+++ app/controllers/api/v1/stories_controller.rb
@@ -9,6 +9,16 @@ class API::V1::StoriesController < ApplicationController
     author_id = story.user_id
     author = User.find(author_id)
     render json: {author: author.username, story: {title: story.title, body: story.body}}
-
   end
+
+  def create
+    @story = Story.new(story_param)
+    @story.save
+    render json: {story: @story}
+  end
+
+  def story_param
+     params.require(:story).permit(:title, :body, :user_id)
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -12,4 +12,14 @@ class API::V1::UsersController < ApplicationController
 
   end
 
+  def create
+    @user = User.new(user_param)
+    @user.save
+    render json: {user: @user}
+  end
+
+  def user_param
+     params.require(:user).permit(:username, :password)
+  end
+
 end
