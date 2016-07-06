d2e33b0d384d3cd3b02a8f8587e9bbb8fbb0bd50
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-07-06 10:55:04 -0600

    CORS CHORES

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://storysmasher.firebaseapp.com','https://storysmasher.firebaseapp.com', 'http://localhost:5000', 'http://localhost:3000'
+    origins 'https://storysmasher.firebaseapp.com','https://storysmasher.com', 'http://localhost:5000', 'http://localhost:3000'
 
     resource '*',
       headers: :any,
